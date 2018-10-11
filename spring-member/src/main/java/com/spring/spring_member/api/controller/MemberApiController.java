86bee9619615654bc98111505862e3d481590801
zhanglin <525975661@qq.com>
2018-10-11 18:03:03 +0800

    add info

--- spring-member/src/main/java/com/spring/spring_member/api/controller/MemberApiController.java
+++ spring-member/src/main/java/com/spring/spring_member/api/controller/MemberApiController.java
@@ -1,14 +1,17 @@
 package com.spring.spring_member.api.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberApiController {
 
+	@Value("${server.port}")
+	private String serverPort;
 	@GetMapping("/getMember")
 	public String  getMember() {
 		
-		return "this is member, springcloud2";
+		return "this is member, springcloud2.端口号:" + serverPort;
 	}
 }
